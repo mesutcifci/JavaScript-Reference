e147d9893fffbe13ad21d1bfe46ce487395f06cf
Mesut Çifci <mesutcifci97@gmail.com>
2020-09-04 09:26:05 +0300

    var vs let

--- var-vs-let.js
+++ var-vs-let.js
@@ -6,14 +6,26 @@ if(true) {
 
 console.log(x); // result = 10
 console.log(y); // result = 20
-console.log(z); // ReferenceError: z is not defined
+// console.log(z); // ReferenceError: z is not defined
 
-num1 = 5;
+num1 = 1;
 num1 = 10;      // OK
 
-var num2 = 111;
-var num2 = 112; // OK
+var num2 = 2;
+var num2 = 20; // OK
 
-let num3 = 14;
-let num3 = 28;  // SyntaxError: redeclaration of let num3
+let num3 = 3;
+// let num3 = 30;  // SyntaxError: redeclaration of let num3
 
+
+console.log(num4); // result = undefined
+var num4 = 4;
+
+(function() {
+console.log(num4); // result = undefined
+var num4 = 40;
+})();
+
+
+// console.log(num5); // ReferenceError: can't access lexical declaration `num5' before initialization
+let num5 = 5;
