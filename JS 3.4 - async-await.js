45612c548f3e22f3c4e1efa14bd52fa55c301ec4
Mesut Çifci <mesutcifci97@gmail.com>
2020-10-31 19:42:02 +0300

    async/await

---
+++ JS 3.4 - async-await.js
@@ -0,0 +1,52 @@
+/*
+async function example() {
+    return 'text';
+}
+
+console.log(typeof (example())); // object
+
+// Promise { "fulfilled" }
+// <state>: "fulfilled"
+// <value>: "text" 
+
+console.log(example());
+
+example().then(
+    resolve => {
+        console.log(resolve); //text
+    }
+);
+*/
+
+/*
+
+async function sayHello() {
+
+    let promise = new Promise((resolve) => {
+        setTimeout(() => resolve("Hello"), 3000)
+    });
+
+    let result = await promise; // wait for the promise to be resolved
+
+    console.log(result);
+}
+
+console.log("0");
+
+sayHello();
+
+console.log("1");
+
+// result = 0 1 Hello
+*/
+
+/*
+async function getData() {
+    return 'Data';
+}
+
+
+const firtProgress = ( ) => await getData().then(console.log);
+firtProgress();
+console.log("playing video");
+*/
