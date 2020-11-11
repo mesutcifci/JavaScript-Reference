45825c6c1e880b194e04ed64846a0d339c8bee16
Mesut Çifci <mesutcifci97@gmail.com>
2020-11-11 05:34:52 +0300

    code format changed DOM_5 files updated DOM_8 created

--- JS 3.8.5 - DOM_5.js
+++ JS 3.8.5 - DOM_5.js
@@ -1,26 +1,62 @@
-"use strict"
+"use strict";
 
-console.log(document.body.nodeType); // 1 corresponds  element nodes
+console.log("\n");getComputedStyle
+console.log("***************************************************");
+console.log("*   nodeType - tagName - nodeName - setAttribute  *");
+console.log("* getAttribute  -  removeAttribute - hasAttribute *");
+console.log("*                getComputedStyle                 *");
+console.log("***************************************************");
+console.log("\n");
+
+console.log(document.body.nodeType);            // 1 corresponds  element nodes
 console.log(document.body.firstChild.nodeType); // 3 corresponds text nodes
 console.log("\n");
 
-console.log(document.body.firstChild.tagName);  // udefined
+console.log(document.body.firstChild.tagName);  // undefined
 console.log(document.body.firstChild.nodeName); // #text
 console.log("\n");
 
 document.body.obj = {
-    name:'my object',
+    name: "my object",
     printName() {
         console.log(this.name);
-    }
-}
+    },
+};
 
-
-console.log(document.body.obj.name);        // my object
-console.log(document.body.obj.printName()); // my object  
+console.log(document.body.obj.name); // my object
+document.body.obj.printName();       // my object
 console.log("\n");
 
+let div1 = document.getElementById("div1");
+let div2 = document.getElementById("div2");
+
+console.log(div1.getAttribute("id"));    // div1
+console.log(div1.hasAttribute("width")); // false because width is not DOM attribute
+console.log(div1.hasAttribute("id"));    // true
+console.log("\n");
+
+div2.setAttribute("style", "display: none;");
+div2.setAttribute(
+    "style",
+    "width: 100px; height:100px; background-color:green;"
+); // owerride
+
+// non-standart attribute
 document.body.setAttribute("custom", "custom attribute");
 console.log(document.body.getAttribute("custom")); // custom attribute
 document.body.removeAttribute("custom");
 console.log(document.body.getAttribute("custom")); // null
+console.log("\n");
+
+let span1 = document.getElementById("span1");
+
+console.log(div1.style.color); // brown
+
+// <empty string> because The style property operates only on the value of the "style" attribute,
+// without any CSS cascade.
+console.log(div1.style.width); 
+console.log(span1.style.width); // <empty string>
+console.log("\n");
+
+let computed = getComputedStyle(span1);
+console.log(computed.width); // 100px
---
+++ JS 3.8.8 - DOM_8.js
@@ -0,0 +1,44 @@
+"use strict";
+
+console.log("\n");
+console.log("***********************************************************");
+console.log("*  getElementsByClassName - classList - className  style  *");
+console.log("***********************************************************");
+console.log("\n");
+
+let divs = document.body.getElementsByClassName("redBox");
+
+let div1 = divs[0];
+let div2 = divs[1];
+console.log(div1); // <div class="redBox">
+
+div1.classList.add("box");
+div2.classList.add("box");
+
+let div3 = document.createElement("div");
+div3.className = "greenBox";
+div3.append("3");
+document.body.append(div3);
+
+setTimeout(() => div3.classList.remove("greenBox"), 2000);
+
+// adds the class if it doesn’t exist, otherwise removes it.
+// so this code removes div2's className
+console.log(div2.classList.toggle("redBox")); // false
+
+console.log(div1.classList.contains("box")); // true
+
+document.body.style.backgroundColor = "blue";
+
+let span = document.createElement("span");
+
+// such assignment removes all existing styles
+span.style.cssText = `
+width: 100px;
+height: 100px;
+display: inline-block;
+background-color: rgb(220, 120, 60);
+margin: 20px;
+`;
+
+document.body.append(span);
